89d7e43589d888e6f382ea23e8b0c636338c345e
Serg <tumanovser@mail.ru>
2020-11-18 12:42:04 +0300

    add button

---
+++ script.js
@@ -0,0 +1,4 @@
+const button = document.querySelector(".spinnerRun");
+const spinner = document.querySelector(".spinner");
+
+button.addEventListener("click", () => spinner.classList.toggle("none"));
